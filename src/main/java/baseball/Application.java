06e88a8d9883a884ea9a1cd8994f3af9ae4376ca
Dongwoongkim <rlaehddnd0422@naver.com>
2023-10-19 16:15:59 +0900

    feat : 랜덤한 숫자 3개 생성 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,9 @@
 package baseball;
 
+import baseball.util.NumberGenerator;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
