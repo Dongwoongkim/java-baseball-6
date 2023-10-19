a4b8d551ce237cb18a35704f7c06726a1789e1ed
Dongwoongkim <rlaehddnd0422@naver.com>
2023-10-19 16:59:26 +0900

    docs : 프로그램 Flow Chart 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,5 @@
 package baseball;
 
-import baseball.domain.User;
-import baseball.util.NumberGenerator;
-
-import java.util.List;
-
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
