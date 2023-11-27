55969f0114b2f3c7e694f2634b05ae0493acbd9e
Dongwoongkim <rlaehddnd0422@naver.com>
2023-11-27 15:35:35 +0900

    feat : 숫자 야구 게임을 진행하는 컨트롤러 구현

---
+++ src/main/java/baseball/controller/BaseballGameController.java
@@ -0,0 +1,19 @@
+package baseball.controller;
+
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
+public class BaseballGameController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public BaseballGameController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+    
+    public void run() {
+
+    }
+}
